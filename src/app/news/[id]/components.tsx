5685c6cbbefe487ddcea4ab09e04c22d5163060b
byungi <zeronin2@gmail.com>
2023-09-25 01:13:10 +0900

    oops

--- src/app/news/[id]/components.tsx
+++ src/app/news/[id]/components.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 export function Delete({ action }: { action: () => Promise<void> }) {
     const router = useRouter()
