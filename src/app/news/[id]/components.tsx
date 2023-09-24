d1044cab1b26d299560eda088fbbffe0f7aacc4d
byungi <zeronin2@gmail.com>
2023-09-25 01:12:00 +0900

    wip

--- src/app/news/[id]/components.tsx
+++ src/app/news/[id]/components.tsx
@@ -1,12 +1,15 @@
 'use client'
 
+import { useRouter } from 'next/router'
+
 export function Delete({ action }: { action: () => Promise<void> }) {
+    const router = useRouter()
     return (
         <button
             onClick={async () => {
                 await action()
                 confirm('삭제되었습니다.')
-                location.href = '/news'
+                router.push('/news')
             }}
         >
             삭제
